ed9a538927db002edaeb94c76a433b6e9374b879
Eduarda Lima <me.eduuarda@gmail.com>
2023-05-11 22:12:23 -0400

    First Project

---
+++ InteractiveCard/index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var numberCard = document.getElementById("numberCard").value;
+  document.getElementById("demo").innerHTML = numberCard;
+}
+
+function getName() {
+  var numberCard = document.getElementById("nameCard").value;
+  document.getElementById("name").innerHTML = numberCard;
+}
